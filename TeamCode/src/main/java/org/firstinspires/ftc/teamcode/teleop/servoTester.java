06b6b4682565472c766429a845fd21077a91d682
C4VSWIN <hown07@icloud.com>
2025-10-11 11:21:41 -0700

    servo tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/servoTester.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.teleop;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "servo tester")
+public class servoTester extends Methods{
+    @Override
+    public void runOpMode(){
+        double servoPosition = 0;
+        Servo testServo = hardwareMap.get(Servo.class, "test servo");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.aWasPressed()) {
+                servoPosition += 0.01;
+            } else if (gamepad1.bWasPressed()) {
+                servoPosition -= 0.01;
+            }
+
+            testServo.setPosition(servoPosition);
+
+            telemetry.addData("servo position", servoPosition);
+            telemetry.update();
+
+        }
+    }
+
+
+}
