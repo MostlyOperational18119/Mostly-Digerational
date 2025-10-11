548ab2229d58d6506966214739d15e3d6fda18e3
MostlyOperational18119 <64237204+MostlyOperational18119@users.noreply.github.com>
2025-10-10 23:56:34 -0700

    aaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/whatever_the_hell_you_want.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/whatever_the_hell_you_want.java
@@ -2,22 +2,46 @@ package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "alex")
 public class whatever_the_hell_you_want extends Methods {
     @Override
     public void runOpMode(){
         float turn, strafe, forwards;
+        int launchAngle = 0;
         DcMotor motorFL = hardwareMap.get(DcMotor.class, "motorFL");
         DcMotor motorFR = hardwareMap.get(DcMotor.class, "motorFR");
+        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
         DcMotor motorBL = hardwareMap.get(DcMotor.class, "motorBL");
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
         DcMotor motorBR = hardwareMap.get(DcMotor.class, "motorBR");
+        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        DcMotor motorLauncher = hardwareMap.get(DcMotor.class, "motorLauncher");
+        motorLauncher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorLauncher.setTargetPosition(0);
+        motorLauncher.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //Servo littleArm = hardwareMap.get(Servo.class, "littleArm");
 
         waitForStart();
+        motorLauncher.setPower(0.5);
         while(opModeIsActive()) {
             turn = gamepad1.right_stick_x;
             strafe = gamepad1.left_stick_x;
             forwards = -gamepad1.left_stick_y;
+            /*if(gamepad1.aWasPressed()){
+                littleArm
+            }*/
+
+            if(gamepad1.dpad_down) {
+                launchAngle -= 5;
+            } else if (gamepad1.dpad_up) {
+                launchAngle += 5;
+            }
+
+            motorLauncher.setTargetPosition(launchAngle);
 
             motorFL.setPower(forwards + strafe + turn);
             motorFR.setPower(forwards - strafe - turn);
