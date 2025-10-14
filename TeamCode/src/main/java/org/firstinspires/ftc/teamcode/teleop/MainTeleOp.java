1c1280585ab1486f5617ba4d8c6d7c4b034011fc
SaarangShaikh <saarangpshaikh@gmail.com>
2025-10-14 10:08:30 -0700

    Launch state machine, some functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/MainTeleOp.java
@@ -13,21 +13,38 @@ public class MainTeleOp extends Methods {
     public void runOpMode() {
         initialize();
         waitForStart();
-        detectAprilTag();
+        Launch launch = new Launch(this);
         while (opModeIsActive()) {
             turn = gamepad1.right_stick_x;
             strafe = gamepad1.left_stick_x;
             forwards = -gamepad1.left_stick_y;
 
-            fire = gamepad2.a;
-            transferToggle = gamepad2.b;
-            cycleLeft = gamepad2.dpad_left;
-            cycleRight = gamepad2.dpad_right;
+            fire = gamepad2.aWasPressed();
+            transferToggle = gamepad2.bWasPressed();
+            cycleLeft = gamepad2.dpadLeftWasPressed();
+            cycleRight = gamepad2.dpadRightWasPressed();
 
-
+            detectAprilTag();
             drive();
-            
+            launch.update();
+
+            if (fire) {
+                launch.startLaunch();}
             intake.setPower(gamepad2.right_trigger);
+
+            switch (ballPosition) {
+                case ONE:
+                    revolver.setPosition(0);
+                    break;
+                case TWO:
+                    revolver.setPosition(0.1);
+                    break;
+                case THREE:
+                    revolver.setPosition(0.2);
+                    break;
+            }
+
+            telemetry.update();
         }
     }
 }
