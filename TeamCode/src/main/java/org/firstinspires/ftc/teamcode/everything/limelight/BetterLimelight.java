30931b70c2ba99ab54ed874dd530919eda8a631d
IShaderDeviceMgr <49534369+IShaderDeviceMgr@users.noreply.github.com>
2025-11-28 14:02:33 -0800

    Start aggressively shoving my Limelight stuff into the main TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everything/limelight/BetterLimelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everything/limelight/BetterLimelight.java
@@ -1,17 +1,80 @@
 package org.firstinspires.ftc.teamcode.everything.limelight;
 
+import android.util.Log;
+
+import org.firstinspires.ftc.teamcode.everything.Indexer;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class BetterLimelight {
     DataInputStream in = null;
     DataOutputStream out = null;
     Socket socket;
+    int chosenGoal = 0;
+    Indexer.BallColor[] balls = new Indexer.BallColor[9];
 
-    BetterLimelight() throws IOException {
-        socket = new Socket("172.29.0.21", 8888);
+    public BetterLimelight() throws IOException {
+        connect();
+    }
 
+    public boolean update() {
+        try {
+            Log.i("BetterLimelight", "hello 3");
+
+            if (in.available() > 0) {
+                Log.i("BetterLimelight", String.format("hello 3 yes input %d", in.available()));
+
+                byte[] data = new byte[in.available()];
+                in.readFully(data);
+
+                if (new String(Arrays.copyOf(data, 5)).equals("Hello")) { // new byte[] { 0x48,0x65,0x6C,0x6C,0x6F }
+                    Log.i("BetterLimelight", "LL says hi :D");
+                } else {
+                    Log.i("BetterLimelight", "hello 3 read data");
+
+                    ToRobotMsg message = new ToRobotMsg(data);
+
+                    switch (message.type) {
+                        case Connected:
+                            Log.i("BetterLimelight", "LimeLight says hi");
+                            break;
+                        case CurrentData:
+
+//                                    ballGoalColor = message.otherData[0];
+                            balls = (Indexer.BallColor[]) message.results.get(ToRobotMsg.ResultType.BallLine);
+                            break;
+                        default:
+                            Log.i("BetterLimelight", "what");
+                    }
+                }
+
+                Log.i("BetterLimelight", "hello 3 done loop");
+
+            } else {
+                Log.i("BetterLimelight", "hello 4 no");
+
+                ToLimelightMsg message = new ToLimelightMsg((byte) chosenGoal);
+
+                Log.i("BetterLimelight", Arrays.toString(message.getData()));
+
+                out.write(message.getData());
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e("BetterLimelight", e.getLocalizedMessage());
+            
+            return false;
+        }
+    }
+    
+    public void connect() throws IOException {
+        socket = new Socket("172.29.0.1", 8888);
+
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
     }
 }
