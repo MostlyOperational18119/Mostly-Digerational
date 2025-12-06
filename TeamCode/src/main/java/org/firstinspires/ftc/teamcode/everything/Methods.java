24f3cf88ea64060add7a80d99d7e738733fba2f9
IShaderDeviceMgr <49534369+IShaderDeviceMgr@users.noreply.github.com>
2025-12-05 17:04:16 -0800

    Slightly better AprilTag auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everything/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/everything/Methods.java
@@ -39,9 +39,10 @@ public abstract class Methods extends LinearOpMode {
     double goalX;
     double goalY = 144;
 
-    public static final int[] TAG_21_PATTERN = {};
-    public static final int[] TAG_22_PATTERN = {};
-    public static final int[] TAG_23_PATTERN = {};
+    // 0 is green, 1 is purple
+    public static final int[] TAG_21_PATTERN = {0, 1, 1};
+    public static final int[] TAG_22_PATTERN = {1, 0, 1};
+    public static final int[] TAG_23_PATTERN = {1, 1, 0};
 
     //initializes all the hardware and the apriltag detection
     public void initialize() {
