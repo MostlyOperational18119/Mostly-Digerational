d86e827485de10c6daf563736dc645d55669d4c4
Icho2468 <isaac.h.cho@gmail.com>
2025-12-15 18:44:31 -0800

    Update Outtake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Outtake.java
@@ -8,13 +8,15 @@ import org.firstinspires.ftc.teamcode.Robot.Robot;
 
 public class Outtake {
 
-    public DcMotorEx rotate, outtakeMotor1, outtakeMotor2;
-    public Servo hood;
+    public DcMotorEx outtakeMotor1, outtakeMotor2;
+    public Servo hood, clutch, rotate1, rotate2;
 
     public void init (HardwareMap hwMap) {
         outtakeMotor2 = hwMap.get(DcMotorEx.class, "outtake_motor_2");
         outtakeMotor1 = hwMap.get(DcMotorEx.class, "outtake_motor_1");
-        rotate = hwMap.get(DcMotorEx.class, "rotate");
+        rotate1 = hwMap.get(Servo.class, "rotate1");
+        rotate2 = hwMap.get(Servo.class, "rotate2");
+        clutch = hwMap.get(Servo.class, "clutch");
         hood = hwMap.get(Servo.class, "hood");
     }
 
