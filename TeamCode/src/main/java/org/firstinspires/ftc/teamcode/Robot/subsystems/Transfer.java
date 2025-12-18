348b7e2adbdb4d6d75c9e6e9b676938f075d7ad9
Icho2468 <isaac.h.cho@gmail.com>
2025-12-17 18:22:21 -0800

    cleaned up transfer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
@@ -25,46 +25,58 @@ public class Transfer {
     }
 
     public void launch (int[] index, int[] idealChamber, int chamberNum) {
+        //declare ideal array
         int[] ideal = new int[3];
 
+        //create idea array by copying the chamber array past filled slots
         System.arraycopy(idealChamber, chamberNum, ideal, 0,  3);
 
+        //iterate through the ideal array to launch the correct ball for pattern
         for (int i = 0; i < 3; i++) {
             long startTime, currentTime;
 
+            //set the start time at the start of the loop
             startTime = System.currentTimeMillis();
 
+            //check if the first slot matches what we want to shoot
             if (index[0] == ideal[i]) {
                 slot0.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while ((currentTime-startTime) < FLICK);
+                timer(startTime, FLICK);
                 slot0.setPosition(DOWN_POS);
 
-
                 index[0] = 0;
+                timer(startTime, WAIT);
+
+            //check if the second slot matches what we want to shoot
             } else if (index[1] == ideal[i]) {
                 slot1.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while ((currentTime-startTime) < FLICK);
+                timer(startTime, FLICK);
                 slot1.setPosition(DOWN_POS);
 
                 index[1] = 0;
+                timer(startTime, WAIT);
+
+            //check if the third slot matches what we want to shoot
             } else if (index[2] == ideal[i]) {
                 slot2.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while ((currentTime-startTime) < FLICK);
+                timer(startTime, FLICK);
                 slot2.setPosition(DOWN_POS);
 
                 index[2] = 0;
+                timer(startTime, WAIT);
             }
 
-            do {
-                currentTime = System.currentTimeMillis();
-            } while ((currentTime-startTime) < WAIT);
+
+            //wait until flicker is down before launching
         }
 
+    }
+
+    private void timer (long startTime, long target) {
+        long currentTime, delta;
+        do {
+            currentTime = System.currentTimeMillis();
+            delta = currentTime-startTime;
+        } while (delta < target);
     }
 }
