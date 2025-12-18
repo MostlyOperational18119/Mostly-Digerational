ec2514315b263283a023ea6f8f981d38757b0eb5
18119 Mostly Operational <64237204+MostlyOperational18119@users.noreply.github.com>
2025-12-17 19:35:08 -0800

    changed driving stick direction, motor directions, and the odo pod offsets are in the pedropathing subfolder in the correct place btw im seeing lots of warnings about unused stuff so idk if we wanna clear that stuff out or nah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/competitionOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/competitionOpMode.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.Robot.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Robot.subsystems.Drivetrain;
-
+@TeleOp(name="drivetrain testing")
 public class competitionOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,8 +17,8 @@ public class competitionOpMode extends LinearOpMode {
         while (opModeIsActive()) {
 
             //drivetrain controller input variable declarations
-            float y = -gamepad1.left_stick_y; // y stick is reversed
-            float x = gamepad1.left_stick_x;
+            float y = gamepad1.left_stick_y; // y stick is not reversed
+            float x = -gamepad1.left_stick_x;
             float rx = gamepad1.right_stick_x;
             
             Drivetrain.drive(y, x, rx);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drivetrain.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Robot.subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
@@ -15,9 +16,13 @@ public class Drivetrain {
 
     public static void init (HardwareMap hwMap) {
         frontLeft = hwMap.get(DcMotor.class, "front_left");
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRight = hwMap.get(DcMotor.class, "front_Right");
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft = hwMap.get(DcMotor.class, "back_left");
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backRight = hwMap.get(DcMotor.class, "back_right");
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     static public void drive(float y, float x, float rx) {
