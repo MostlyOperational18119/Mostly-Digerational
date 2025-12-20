4cedb893f055a8552e01f8c04c1001e30e4836d1
Icho2468 <isaac.h.cho@gmail.com>
2025-12-20 13:10:29 -0800

    h

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
@@ -8,10 +8,10 @@ public class Transfer {
     private static final long WAIT = 300, FLICK = 100;
     private static final double UP_POS_0 = 0,DOWN_POS_0 = 0, UP_POS_1 = 0,DOWN_POS_1 = 0, UP_POS_2 = 0,DOWN_POS_2 = 0;
     public static Servo slot0, slot1, slot2;
-    static int launchNum = 0;
-    static int[] ideal = new int[3];
     static long startTime;
+    static int[] ideal;
 
+    static int launchNum = 0;
 
     public static enum State {
         LAUNCH,
@@ -58,13 +58,16 @@ public class Transfer {
 
                 startTime = System.currentTimeMillis();
 
-                if (launchServo(ideal, slots, idealChamber, chamberNum) == 0) {
+
+
+
+                if (launchServo(ideal, slots) == 0) {
                     slot0.setPosition(UP_POS_0);
                     upServo = 0;
-                } else if (launchServo(ideal, slots, idealChamber, chamberNum) == 1) {
+                } else if (launchServo(ideal, slots) == 1) {
                     slot1.setPosition(UP_POS_1);
                     upServo = 1;
-                } else if (launchServo(ideal, slots, idealChamber, chamberNum) == 2) {
+                } else if (launchServo(ideal, slots) == 2) {
                     slot2.setPosition(UP_POS_2);
                     upServo = 2;
                 }
@@ -74,6 +77,8 @@ public class Transfer {
                 break;
 
             case IDLE:
+                int emptySlotNum = 0;
+
                 if (upServo == 0) {
                     slot0.setPosition(DOWN_POS_0);
                 } else if (upServo == 1) {
@@ -82,30 +87,42 @@ public class Transfer {
                     slot2.setPosition(DOWN_POS_2);
                 }
 
+                for (int i = 0; i < slots.length; i++) {
+                    if (slots[i] == 0) {
+                        emptySlotNum++;
+                    }
+                }
+                if (emptySlotNum == slots.length) {
+                    launchNum = 0;
+                }
+
                 chamberCheck(idealChamber, chamberNum);
                 break;
         }
 
     }
 
-    private static int launchServo (int[] ideal, int[] index, int[] idealChamber, int chamberNum) {
+    private static int launchServo (int[] ideal, int[] slots) {
 
-        if (index[0] == ideal[launchNum]) {
-            index[0] = 0;
-            return 0;
-        } else if (index[1] == ideal[launchNum]) {
-            index[2] = 0;
-            return 2;
-        } else if (index[2] == ideal[launchNum]) {
-            index[2] = 0;
-            return 2;
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == ideal[launchNum]) {
+                return i;
+            }
         }
         return -1;
     }
 
+
     public static void chamberCheck (int[] idealChamber, int chamberNum) {
-        for (int i = 0; i < 3; i++) {
+        if (chamberNum > 6) {
+            ideal = new int[9 - chamberNum];
+        } else {
+            ideal = new int[3];
+        }
+
+        for (int  i = 0; i < ideal.length; i++) {
             ideal[i] = idealChamber[i+chamberNum];
         }
+
     }
 }
