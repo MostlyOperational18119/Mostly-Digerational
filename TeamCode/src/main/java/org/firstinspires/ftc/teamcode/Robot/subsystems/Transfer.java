ec4ae33449433ecb9822c53997409a333b23a66b
Icho2468 <isaac.h.cho@gmail.com>
2025-12-20 09:04:12 -0800

    Update Transfer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Transfer.java
@@ -8,13 +8,18 @@ import java.util.TimerTask;
 
 public class Transfer {
 
-    private static final long WAIT = 300;
-    private static final long FLICK = 100;
-    private static final double UP_POS = 1;
-    private static final double DOWN_POS = 0;
+    private static final long WAIT = 300, FLICK = 100;
+    private static final double UP_POS_0 = 0,DOWN_POS_0 = 0, UP_POS_1 = 0,DOWN_POS_1 = 0, UP_POS_2 = 0,DOWN_POS_2 = 0;
     public Servo slot0, slot1, slot2;
+    int launchNum = 0;
+    int[] ideal = new int[3];
 
+    public enum State {
+        LAUNCH,
+        IDLE
+    }
 
+    public State currentState;
 
     public void init (HardwareMap hwMap) {
 
@@ -22,65 +27,70 @@ public class Transfer {
         slot1 = hwMap.get(Servo.class, "slot_2_servo");
         slot2 = hwMap.get(Servo.class, "slot_3_servo");
 
+        currentState = State.IDLE;
     }
 
-    public void launch (int[] index, int[] idealChamber, int chamberNum) {
-        //declare ideal array
-        int[] ideal = new int[3];
+    public void startLaunch (currentState) {
+        
+    }
+    public void update (int[] index, int[] idealChamber, int chamberNum) {
+        int upServo = -1;
+        long startTime;
+        switch(currentState) {
+            case LAUNCH:
 
-        //create idea array by copying the chamber array past filled slots
-        System.arraycopy(idealChamber, chamberNum, ideal, 0,  3);
+                if ((System.currentTimeMillis() - startTime) < WAIT) {
+                    currentState = State.IDLE;
+                }
 
-        //iterate through the ideal array to launch the correct ball for pattern
-        for (int i = 0; i < 3; i++) {
-            long startTime, currentTime;
+                if (launchServo(ideal, index, idealChamber, chamberNum) == 0) {
+                    slot0.setPosition(UP_POS_0);
+                    upServo = 0;
+                } else if (launchServo(ideal, index, idealChamber, chamberNum) == 1) {
+                    slot1.setPosition(UP_POS_1);
+                    upServo = 1;
+                } else if (launchServo(ideal, index, idealChamber, chamberNum) == 2) {
+                    slot2.setPosition(UP_POS_2);
+                    upServo = 2;
+                }
+                startTime = System.currentTimeMillis();
+                launchNum++;
+                currentState = State.IDLE;
+                break;
 
-            //set the start time at the start of the loop
-            startTime = System.currentTimeMillis();
+            case IDLE:
+                if (upServo == 0) {
+                    slot0.setPosition(DOWN_POS_0);
+                } else if (upServo == 1) {
+                    slot1.setPosition(DOWN_POS_1);
+                } else if (upServo == 2) {
+                    slot2.setPosition(DOWN_POS_2);
+                }
 
-            //check if the first slot matches what we want to shoot
-            if (index[0] == ideal[i]) {
-                slot0.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < WAIT);
-                slot0.setPosition(DOWN_POS);
-
-                index[0] = 0;
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < WAIT);
-
-            //check if the second slot matches what we want to shoot
-            } else if (index[1] == ideal[i]) {
-                slot1.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < FLICK);
-                slot1.setPosition(DOWN_POS);
-
-                index[1] = 0;
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < WAIT);
-
-            //check if the third slot matches what we want to shoot
-            } else if (index[2] == ideal[i]) {
-                slot2.setPosition(UP_POS);
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < FLICK);
-                slot2.setPosition(DOWN_POS);
-
-                index[2] = 0;
-                do {
-                    currentTime = System.currentTimeMillis();
-                } while (currentTime-startTime < WAIT);
-            }
-
-
-            //wait until flicker is down before launching
+                chamberCheck(idealChamber, chamberNum);
+                break;
         }
 
+    }
+
+
+
+    private int launchServo (int[] ideal, int[] index, int[] idealChamber, int chamberNum) {
+
+        if (index[0] == ideal[launchNum]) {
+            index[0] = 0;
+            return 0;
+        } else if (index[1] == ideal[launchNum]) {
+            index[2] = 0;
+            return 2;
+        } else if (index[2] == ideal[launchNum]) {
+            index[2] = 0;
+            return 2;
+        }
+        return -1;
+    }
+
+    public void chamberCheck (int[] idealChamber, int chamberNum) {
+        System.arraycopy(idealChamber, chamberNum, ideal, 0,  3);
     }
 }
