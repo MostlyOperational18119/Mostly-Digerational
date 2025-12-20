7a1fb34be8c1a4c995b259ab22992938222cffd4
Icho2468 <isaac.h.cho@gmail.com>
2025-12-20 11:16:20 -0800

    Autoaim logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Outtake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Outtake.java
@@ -8,34 +8,51 @@ public class Outtake {
 
     static private DcMotorEx outtakeMotor1, outtakeMotor2, rotate;
     private Servo hood, clutch, rotateServo;
-    private final double CLUTCH_IN = .75;
-    private final double CLUTCH_OUT = .75;
-    private final double BACK_HOOD = .75;
-    private final double FRONT_HOOD = .25;
-    private final int CHAMBER_OFFSET = 200;
-    private final int OFFSET = 200;
-
-    private double speed;
+    private final double CLUTCH_IN = .75, CLUTCH_OUT = .75, BACK_HOOD = .75,FRONT_HOOD = .25, SPEED_DIV = 5;
+    private final int CHAMBER_OFFSET = 200, OFFSET = 200;
+    private static double speed;
 
     public void init (HardwareMap hwMap) {
-        outtakeMotor2 = hwMap.get(DcMotorEx.class, "outtake_motor_2");
-        outtakeMotor1 = hwMap.get(DcMotorEx.class, "outtake_motor_1");
+        outtakeMotor2 = hwMap.get(DcMotorEx.class, "outtakemotor2");
+        outtakeMotor1 = hwMap.get(DcMotorEx.class, "outtakemotor1");
         rotateServo = hwMap.get(Servo.class, "rotate");
         clutch = hwMap.get(Servo.class, "clutch");
         hood = hwMap.get(Servo.class, "hood");
         clutch.setPosition(CLUTCH_OUT);
     }
 
-    public void autoAim (double x, double y, boolean launch) {
-        double target;
-        double dist;
+    public void autoAimBlue (double x, double y, boolean launch) {
+        double target, dist, pos;
+
+        target = Math.atan((144-y)/x);
+
         if (launch) {
-            target = Math.atan((y - OFFSET - CHAMBER_OFFSET)/x);
+            pos = target*(2048/360) + OFFSET + CHAMBER_OFFSET;
         } else {
-            target =  Math.atan((y - OFFSET)/x);
+            pos = target*(2048/360) + OFFSET;
         }
-        dist = Math.pow(x, 2) + Math.pow(y, 2); Math.pow(x, 2);
-        
+        dist = Math.sqrt(Math.pow(x, 2) + Math.pow(144-y, 2));
+
+        speed  = dist/SPEED_DIV;
+
+        rotate.setTargetPosition((int)pos);
+    }
+
+    public void autoAimRed (double x, double y, boolean launch) {
+        double target, dist, pos;
+
+        target = Math.atan((144-y)/144-x);
+
+        if (launch) {
+            pos = target*(2048/360) + OFFSET + CHAMBER_OFFSET;
+        } else {
+            pos = target*(2048/360) + OFFSET;
+        }
+        dist = Math.sqrt(Math.pow(144-x, 2) + Math.pow(144-y, 2));
+
+        speed  = dist/SPEED_DIV;
+
+        rotate.setTargetPosition((int)pos);
     }
 
     public void run (double speed) {
@@ -44,10 +61,14 @@ public class Outtake {
         outtakeMotor1.setVelocity(speed);
         outtakeMotor2.setVelocity(speed);
 
-        do {
-            rpm1 = outtakeMotor1.getVelocity()/28*60;
-            rpm2 = outtakeMotor2.getVelocity()/28*60;
-        } while (rpm1 < 3000 || rpm2 < 3000);
-        clutch.setPosition(CLUTCH_IN);
+        rpm1 = outtakeMotor1.getVelocity()/28*60;
+        rpm2 = outtakeMotor2.getVelocity()/28*60;
+
+        if (rpm1 >= 3000 || rpm2 >= 3000) {
+            clutch.setPosition(CLUTCH_IN);
+        } else {
+            clutch.setPosition(CLUTCH_OUT);
+        }
+
     }
 }
