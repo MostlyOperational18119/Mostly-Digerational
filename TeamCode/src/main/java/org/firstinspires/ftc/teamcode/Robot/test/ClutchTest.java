7de53f216488a6b6c03b553a28a3270d5f015454
Icho2468 <isaac.h.cho@gmail.com>
2025-12-26 15:57:36 -0800

    clutch test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/test/ClutchTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Robot.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Robot.subsystems.Outtake;
+
+import org.firstinspires.ftc.teamcode.Robot.subsystems.Indexer;
+
+@TeleOp(name="clutch")
+public class ClutchTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        Outtake.init(hardwareMap);
+
+        while (opModeIsActive()) {
+            float trigger = gamepad1.right_trigger;
+            if (trigger > .5) {
+                Outtake.run();
+            }
+        }
+    }
+}
