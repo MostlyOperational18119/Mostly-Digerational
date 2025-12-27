75acf2aa23e63b3be458ad72b807198dd9704bf4
Icho2468 <isaac.h.cho@gmail.com>
2025-12-27 09:48:19 -0800

    Outtake states w/o clutch and simplify auto aim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/competitionOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/competitionOpMode.java
@@ -12,6 +12,7 @@ import com.pedropathing.geometry.Pose;
 
 import org.firstinspires.ftc.teamcode.Robot.pedroPathing.Constants;
 import org.firstinspires.ftc.teamcode.Robot.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.Robot.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.Robot.subsystems.Outtake;
 
 @TeleOp(name="drivetrain testing")
@@ -22,10 +23,11 @@ public class competitionOpMode extends LinearOpMode {
         waitForStart();
 
         //placeholders
-        boolean color = false;
+        boolean red = false;
 
         Drivetrain.init(hardwareMap);
         Outtake.init(hardwareMap);
+        Intake.init(hardwareMap);
 
         Pose currentPose;
 
@@ -39,30 +41,25 @@ public class competitionOpMode extends LinearOpMode {
             float y = gamepad1.left_stick_y; // y stick is not reversed
             float x = -gamepad1.left_stick_x;
             float rx = gamepad1.right_stick_x;
+            boolean X = gamepad1.xWasPressed();
             boolean A = gamepad1.aWasPressed();
+            float lt = gamepad1.left_trigger;
 
             int launch = -1;
-            if (A) {
+            if (X) {
                 launch *= -1;
             }
 
             
             Drivetrain.drive(y, x, rx);
-//            Outtake.run();
-//
-//            if (launch > 0) {
-//                if (color) {
-//                    Outtake.autoAimBlue(currentPose.getX(), currentPose.getY(), true);
-//                } else {
-//                    Outtake.autoAimRed(currentPose.getX(), currentPose.getY(), true);
-//                }
-//            } else {
-//                if (color) {
-//                    Outtake.autoAimBlue(currentPose.getX(), currentPose.getY(), false);
-//                } else {
-//                    Outtake.autoAimRed(currentPose.getX(), currentPose.getY(), false);
-//                }
-//            }
+//          Outtake.outtakeUpdate(currentPose.getX(), currentPose.getY(), launch);
+
+
+            if (lt > .5) {
+                Intake.intakeGo();
+            } else {
+                Intake.intakeIdle();
+            }
 
 
 
