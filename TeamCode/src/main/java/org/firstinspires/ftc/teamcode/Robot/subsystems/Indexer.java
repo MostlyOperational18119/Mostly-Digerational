84ac69590d6cb91534f75b1c677025c883e1fa6b
Mostly Laptop #2 <64237204+MostlyOperational18119@users.noreply.github.com>
2025-12-30 11:02:22 -0800

    new indexer logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Indexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Indexer.java
@@ -10,17 +10,22 @@ import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 public class Indexer {
     public static Servo slot0, slot1, slot2;
-    public static double UP_POS_0 = 0.12, DOWN_POS_0 = 0.48, UP_POS_1 = 0.12, DOWN_POS_1 = 0.48, UP_POS_2 = 0.88, DOWN_POS_2 = 0.47;
+    public static double UP_POS_0 = 0.12, DOWN_POS_0 = 0.48, MID_POS_0 = 0.34;
+    public static double UP_POS_1 = 0.12, DOWN_POS_1 = 0.48, MID_POS_1 = 0.34;
+    public static double UP_POS_2 = 0.88, DOWN_POS_2 = 0.48, MID_POS_2 = 0.66;
+
     public enum States {
         LAUNCH,
-        IDLE
+        IDLE,
+        EMPTY
     }
-    public static int[] pattern = new int[] {1, 2, 2}; // change to new int[3];
-    public static States currentState;
+
+    public static int[] pattern = new int[]{1, 2, 2}; // change to new int[3];
+    public static States currentState0, currentState1, currentState2;
     static int currentBall; //ball being launched (0, 1, or 2)
     private static RevColorSensorV3 slot0Sensor, slot1Sensor, slot2Sensor;
 
-    public static void init (HardwareMap hwMap) {
+    public static void init(HardwareMap hwMap) {
         slot0Sensor = hwMap.get(RevColorSensorV3.class, "index0");
         slot1Sensor = hwMap.get(RevColorSensorV3.class, "index1");
         slot2Sensor = hwMap.get(RevColorSensorV3.class, "index2");
@@ -34,7 +39,7 @@ public class Indexer {
         slot2.setPosition(DOWN_POS_2);
     }
 
-    public static int[] slotColors () {
+    public static int[] slotColors() {
 
         //green = 2, purple = 1, empty = 0;
         double hue0, hue1, hue2;
@@ -46,7 +51,7 @@ public class Indexer {
         NormalizedRGBA slot2Colors = slot2Sensor.getNormalizedColors();
         hue2 = JavaUtil.colorToHue(slot2Colors.toColor());
 
-        int[]  slots = new int[3];
+        int[] slots = new int[3];
 
         //0 slot
         if (180 >= hue0 && hue0 >= 150) {
@@ -78,49 +83,40 @@ public class Indexer {
         return slots;
     }
 
-    //Transfer Part Duex import
-    public static void startLaunch () {
-        currentState = States.LAUNCH;
+    public static void updateSlot0 () {
+        switch (currentState0) {
+            case LAUNCH:
+                slot0.setPosition(UP_POS_0);
+                break;
+            case IDLE:
+                slot0.setPosition(DOWN_POS_0);
+                break;
+            case EMPTY:
+                slot0.setPosition(MID_POS_0);
+                break;
+        }
     }
 
-    public static void update (int[] slots) {
-        switch(currentState) {
-            case LAUNCH:
+    //Transfer Part Duex import
+    public static void startLaunch0() {
+        currentState0 = States.LAUNCH;
+    }
+    public static void startLaunch1() {
+        currentState1 = States.LAUNCH;
+    }
+    public static void startLaunch2() {
+        currentState2 = States.LAUNCH;
+    }
 
-                currentBall = nextBall(slots, 6, pattern); //placeholder chamberNum
-                if (currentBall != -1) {
-                    switch (currentBall) {
-                        case 0:
-                            slot0.setPosition(UP_POS_0);
-                            break;
-                        case 1:
-                            slot1.setPosition(UP_POS_1);
-                            break;
-                        case 2:
-                            slot2.setPosition(UP_POS_2);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-
-
-                currentState = States.IDLE;
-            case IDLE:
-                switch (currentBall) {
-                    case 0:
-                        slot0.setPosition(DOWN_POS_0);
-                        break;
-                    case 1:
-                        slot1.setPosition(DOWN_POS_1);
-                        break;
-                    case 2:
-                        slot2.setPosition(DOWN_POS_2);
-                        break;
-                    default:
-                        break;
-                }
-                break;
+    public static void update(int chamberNum, boolean launch) {
+        int[] slots = slotColors();
+        long startTime = System.currentTimeMillis();
+        if (slots[0] == nextBall(slots, chamberNum, pattern)) {
+            currentState0 = States.LAUNCH;
+        } else if (slots[1] == nextBall(slots, chamberNum, pattern)) {
+            currentState1 = States.LAUNCH;
+        } else if (slots[2] == nextBall(slots, chamberNum, pattern)) {
+            currentState2 = States.LAUNCH;
         }
     }
 
@@ -133,21 +129,7 @@ public class Indexer {
             if (slots[i] == desiredColor) {
                 return i;
             }
-            if (slots[i] == 0) {
-                emptyCount++;
-            }
         }
-
-        if (emptyCount < 3) {
-            for (int i = 0; i < slots.length; i++) {
-                if (slots[i] != 0) {
-                    return i;
-                }
-            }
-        }
-
-
         return -1;
     }
-
 }
