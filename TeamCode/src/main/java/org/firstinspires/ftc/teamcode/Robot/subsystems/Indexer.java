2353a7674024005fd57eec9a8fe924202244038d
Icho2468 <isaac.h.cho@gmail.com>
2025-12-30 14:04:36 -0800

    indexer color testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Indexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Indexer.java
@@ -75,7 +75,18 @@ public class Indexer {
         return slot2Sensor.blue();
     }
 
-    private static int getColor (double green, double red, double blue) {
+    public static String slot0Test () {
+        if (getColor(slot0Sensor.red(), slot0Sensor.green(), slot0Sensor.blue()) == 2) {
+            return "green";
+        } else if (getColor(slot0Sensor.red(), slot0Sensor.green(), slot0Sensor.blue()) ==1 ) {
+            return "purple";
+        } else {
+            return "empty";
+        }
+
+    }
+
+    private static int getColor (double red, double green, double blue) {
         if (green/blue > 1.3 && green/blue < 1.5) {
             return 2;
         } else if (red/green > .9 && red/green < 1.2 && red > 100) {
@@ -87,49 +98,11 @@ public class Indexer {
 
 
     public static int[] slotColors() {
-
-        //green = 2, purple = 1, empty = 0;
-        double hue0, hue1, hue2;
-
-        NormalizedRGBA slot0Colors = slot0Sensor.getNormalizedColors();
-        hue0 = JavaUtil.colorToHue(slot0Colors.toColor());
-        NormalizedRGBA slot1Colors = slot1Sensor.getNormalizedColors();
-        hue1 = JavaUtil.colorToHue(slot1Colors.toColor());
-        NormalizedRGBA slot2Colors = slot2Sensor.getNormalizedColors();
-        hue2 = JavaUtil.colorToHue(slot2Colors.toColor());
-
-//        Log.i("Indexer", String.format("Hue 0: %d", hue0));
-//        Log.i("Indexer", String.format("Hue 1: %d", hue1));
-//        Log.i("Indexer", String.format("Hue 2: %d", hue2));
-
         int[] slots = new int[3];
 
-        //0 slot
-        if (180 >= hue0 && hue0 >= 150) {
-            slots[0] = 2;
-        } else if (300 >= hue0 && hue0 >= 240) {
-            slots[0] = 1;
-        } else {
-            slots[0] = 0;
-        }
+        int
 
-        //1 slot
-        if (180 >= hue1 && hue1 >= 150) {
-            slots[1] = 2;
-        } else if (300 >= hue1 && hue1 >= 240) {
-            slots[1] = 1;
-        } else {
-            slots[1] = 0;
-        }
 
-        //2 slot
-        if (180 >= hue2 && hue2 >= 150) {
-            slots[2] = 2;
-        } else if (300 >= hue2 && hue2 >= 240) {
-            slots[2] = 1;
-        } else {
-            slots[2] = 0;
-        }
 
         return slots;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/test/IndexerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/test/IndexerTest.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Robot.subsystems.Indexer;
 
-@TeleOp(name="indexer/transfer")
+@TeleOp(name="indexerTest")
 public class IndexerTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,17 +16,7 @@ public class IndexerTest extends LinearOpMode {
         waitForStart();
         while (opModeIsActive()) {
 
-            telemetry.addData("Slot 0 Red: ", Indexer.slotColor0Red());
-            telemetry.addData("Slot 0 Green: ", Indexer.slotColor0Green());
-            telemetry.addData("Slot 0 Blue: ", Indexer.slotColor0Blue());
-
-            telemetry.addData("Slot 1 Red: ", Indexer.slotColor1Red());
-            telemetry.addData("Slot 1 Green: ", Indexer.slotColor1Green());
-            telemetry.addData("Slot 1 Blue: ", Indexer.slotColor1Blue());
-
-            telemetry.addData("Slot 2 Red: ", Indexer.slotColor2Red());
-            telemetry.addData("Slot 2 Green: ", Indexer.slotColor2Green());
-            telemetry.addData("Slot 2 Blue: ", Indexer.slotColor2Blue());
+            telemetry.addData("Slot0: ", Indexer.slot0Test());
         }
     }
 
