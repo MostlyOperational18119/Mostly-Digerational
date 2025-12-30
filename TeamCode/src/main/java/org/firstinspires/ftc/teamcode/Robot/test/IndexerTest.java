fada1e74b51d78fb4dfcfd61b47d0ca4bc7c67e7
Icho2468 <isaac.h.cho@gmail.com>
2025-12-30 14:23:15 -0800

    testing testing 123

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/test/IndexerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/test/IndexerTest.java
@@ -20,6 +20,17 @@ public class IndexerTest extends LinearOpMode {
             telemetry.addData("slot0green: ", Indexer.slotColor0Green());
             telemetry.addData("slot0blue: ", Indexer.slotColor0Blue());
             telemetry.addData("Slot0: ", Indexer.slot0Test());
+
+            telemetry.addData("slot1red: ", Indexer.slotColor1Red());
+            telemetry.addData("slot1green: ", Indexer.slotColor1Green());
+            telemetry.addData("slot1blue: ", Indexer.slotColor1Blue());
+            telemetry.addData("Slot1: ", Indexer.slot1Test());
+
+            telemetry.addData("slot2red: ", Indexer.slotColor2Red());
+            telemetry.addData("slot2green: ", Indexer.slotColor2Green());
+            telemetry.addData("slot2blue: ", Indexer.slotColor2Blue());
+            telemetry.addData("Slot2: ", Indexer.slot2Test());
+
             telemetry.update();
         }
     }
