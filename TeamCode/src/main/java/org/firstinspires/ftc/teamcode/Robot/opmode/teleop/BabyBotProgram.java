b9123033070dcaee02b48b3f0262f565bd7aa07d
IShaderDeviceMgr <49534369+IShaderDeviceMgr@users.noreply.github.com>
2026-01-10 11:51:03 -0800

    Make Baby Bot actually decently written

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/BabyBotProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/BabyBotProgram.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.everything.outreach;
+package org.firstinspires.ftc.teamcode.Robot.opmode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,32 +9,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "baby bot teleop")
 public class BabyBotProgram extends LinearOpMode {
     public void runOpMode() {
-        DcMotor driveRight = (DcMotor) this.hardwareMap.get(DcMotor.class, "driveRight");
+        DcMotor driveRight = hardwareMap.get(DcMotor.class, "driveRight");
         driveRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        DcMotor driveLeft = (DcMotor) this.hardwareMap.get(DcMotor.class, "driveLeft");
+        DcMotor driveLeft = hardwareMap.get(DcMotor.class, "driveLeft");
         driveLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        DcMotor rotate = (DcMotor) this.hardwareMap.get(DcMotor.class, "rotate");
+        DcMotor rotate = hardwareMap.get(DcMotor.class, "rotate");
         rotate.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rotate.setTargetPosition(0);
         rotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        rotate.setPower(0.5d);
-        Servo release = (Servo) this.hardwareMap.get(Servo.class, "release");
-        release.setPosition(0.61d);
+        rotate.setPower(0.5);
+        Servo release = hardwareMap.get(Servo.class, "release");
+        release.setPosition(0.61);
         waitForStart();
         while (opModeIsActive()) {
-            double leftY = ((double) (-this.gamepad1.left_stick_y)) * 1.25d;
-            double rightX = ((double) this.gamepad1.right_stick_x) * 0.75d;
-            driveLeft.setPower(Math.max(-1.0d, Math.min(1.0d, leftY - rightX)) / 2.0d);
-            driveRight.setPower(Math.max(-1.0d, Math.min(1.0d, leftY + rightX)) / 2.0d);
-            if (this.gamepad1.rightBumperWasPressed()) {
+            double leftY = (-gamepad1.left_stick_y) * 1.25;
+            double rightX = (gamepad1.right_stick_x) * 0.75;
+            driveLeft.setPower(Math.max(-1.0, Math.min(1.0, leftY - rightX)) / 2.0);
+            driveRight.setPower(Math.max(-1.0, Math.min(1.0, leftY + rightX)) / 2.0);
+            if (gamepad1.rightBumperWasPressed()) {
                 rotate.setTargetPosition(rotate.getTargetPosition() - 25);
-            } else if (this.gamepad1.leftBumperWasPressed()) {
+            } else if (gamepad1.leftBumperWasPressed()) {
                 rotate.setTargetPosition(rotate.getTargetPosition() + 25);
             }
-            if (this.gamepad1.aWasPressed()) {
-                release.setPosition(0.8d);
+            if (gamepad1.aWasPressed()) {
+                release.setPosition(0.8);
                 sleep(300);
-                release.setPosition(0.61d);
+                release.setPosition(0.61);
             }
         }
     }
