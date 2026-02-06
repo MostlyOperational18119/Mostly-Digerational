cc78e81341d802ca8a5e269605ff7bd271a85c5e
SaarangShaikh <saarangpshaikh@gmail.com>
2026-02-06 15:50:55 -0800

    Auto working but without Limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Lift.java
@@ -6,26 +6,30 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Lift {
 
-    private static Servo lift1, lift2;
-    private static double UP = 0.82;
+    public static Servo lift1;
+    private static Servo lift2;
+    public static double UP1 = 0.82;
+    private static double UP2 = 0.66;
+    private static double DOWN1 = 1.0;
+    private static double DOWN2 = 0.86;
+    //UP is inside the robot, DOWN is lifted
     public static void init (HardwareMap hwMap) {
         lift1 = hwMap.get(Servo.class, "lift1");
-        //lift2 = hwMap.get(Servo.class, "lift2");
+        lift2 = hwMap.get(Servo.class, "lift2");
 
         //initialize to start position
-        lift1.setPosition(0.82);
-        //lift2.setPosition(0.82);
+        lift1.setPosition(UP1);
+        lift2.setPosition(UP2);
     }
 
     public static void lift () {
-        double LIFT = 1;
-        lift1.setPosition(LIFT);
-        //lift2.setPosition(LIFT);
+        lift1.setPosition(DOWN1);
+        lift2.setPosition(DOWN2);
     }
 
     public static void unlift(){
-        double UP = 0.82;
-        lift1.setPosition(UP);
+        lift1.setPosition(UP1);
+        lift2.setPosition(UP2);
     }
 
 }
