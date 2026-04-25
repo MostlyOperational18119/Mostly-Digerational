7f507214bf90ced51dd86089b9de05716966d2e4
C4VSWIN <hown07@icloud.com>
2026-04-25 10:52:01 -0700

    Update NewMemberIntro.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/NewMemberIntro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/NewMemberIntro.java
@@ -4,12 +4,12 @@ package org.firstinspires.ftc.teamcode.Robot.opmode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "configurable")
+@TeleOp(name = "new member code")
 public class NewMemberIntro extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        
+
     }
 
 }
