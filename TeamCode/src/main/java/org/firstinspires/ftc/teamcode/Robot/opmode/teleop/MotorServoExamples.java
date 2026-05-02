b20afc1eb71a78bcd7cf74c8e7f85335fce5e79d
C4VSWIN <hown07@icloud.com>
2026-05-02 11:04:12 -0700

    useless things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/opmode/teleop/MotorServoExamples.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Robot.opmode.teleop;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class MotorServoExamples {
+    public DcMotor frontLeft;
+    public Servo arm;
+    public RevColorSensorV3 colorSensor;
+
+}
